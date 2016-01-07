ee8ae349be61d8404c7d0ededf774b03e7b8a41f
Albert Vo <albert.vo@gmail.com>
2016-01-07 15:47:44 -0800

    add dict2.py

---
+++ dict2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+phonebook = {'name':'Adam', 'age':'50'}
+items = [('name','Adam'), ('age','50')]
+
+d = dict(items)
+
+print d
+
+print phonebook
+
+
+mydict = {}
+
+mydict = {'host':'10.10.10.10', 'hcount':'0', 'lastaccess':'1-7-16'}
+
+print mydict
+print mydict.keys()
+print mydict.values()
